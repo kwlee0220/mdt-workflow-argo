b531864ea68d58832407805a8a90253272224c21
Kang-Woo Lee <kwlee@etri.re.kr>
2025-07-23 16:07:25 +0900

    Enforce RFC 1123 compliance for task names in ArgoTaskDescriptor

--- src/main/java/mdt/workflow/argo/ArgoTaskDescriptor.java
+++ src/main/java/mdt/workflow/argo/ArgoTaskDescriptor.java
@@ -8,10 +8,11 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 
+import lombok.NonNull;
+
 import utils.func.FOption;
 import utils.stream.FStream;
 
-import lombok.NonNull;
 import mdt.model.MDTModelSerDe;
 
 
@@ -30,7 +31,7 @@ public class ArgoTaskDescriptor {
 								@JsonProperty("template") @NonNull String template,
 								@JsonProperty("dependencies") Set<String> dependencies) {
 		Preconditions.checkArgument(name != null, "Null name");
-		Preconditions.checkArgument(template != null, "Null template");
+		Preconditions.checkArgument(isValidRfc1123Name(name), "Task name '%s' is not valid according to RFC 1123", name);
 		
 		this.m_name = name;
 		this.m_template = template;
@@ -53,6 +54,23 @@ public class ArgoTaskDescriptor {
 	public String toString() {
 		String depsStr = FStream.from(FOption.getOrElse(m_dependencies, Set.of())).join(", ");
 		return String.format("%s (dependents: {%s})", m_name, depsStr);
+	}
+
+	/**
+	 * Validates if the given name complies with RFC 1123 DNS subdomain naming
+	 * conventions. RFC 1123 allows names with: - Only lowercase alphanumeric
+	 * characters, '-' or '.' - Must start and end with an alphanumeric character -
+	 * Maximum length of 253 characters
+	 */
+	private boolean isValidRfc1123Name(String name) {
+		if ( name == null || name.isEmpty() || name.length() > 253 ) {
+			return false;
+		}
+
+		// RFC 1123 pattern: labels separated by dots, each label consisting of
+		// alphanumeric chars or hyphens, starting and ending with alphanumeric
+		String pattern = "^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$";
+		return name.matches(pattern);
 	}
 	
 	public static final void main(String... args) throws Exception {
