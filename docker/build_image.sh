c7492d051ed3fc16100990bb66547dc3330176a1
Kang-Woo Lee <kwlee@etri.re.kr>
2025-11-07 10:45:05 +0900

    Docker image 생성 스크립트 작성.

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-docker image rmi kwlee0220/mdt-workflow-argo
+MDT_WORKFLOW_HOME=$MDT_HOME/mdt-workflow-argo
+MDT_BUILD_VERSION=1.2.1
 
-cp ../build/libs/mdt-workflow-argo-1.0.0-all.jar mdt-workflow-argo.jar
+docker image rmi kwlee0220/mdt-workflow-argo:$MDT_BUILD_VERSION
 
-docker build -t kwlee0220/mdt-workflow-argo:latest .
+cp $MDT_WORKFLOW_HOME/mdt-workflow-argo-all.jar mdt-workflow-argo-all.jar
 
-rm mdt-workflow-argo.jar
+docker build -t kwlee0220/mdt-workflow-argo:$MDT_BUILD_VERSION .
+# docker push kwlee0220/mdt-workflow-argo:$MDT_BUILD_VERSION
+
+rm mdt-workflow-argo-all.jar
